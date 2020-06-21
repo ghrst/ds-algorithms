1d8344b31d754821e36e24150e6ac0475d5d8bf1
Gholamreza Sabery Tabrizy <reza_sabery_89@yahoo.com>
2020-06-21 11:19:51 +0430

    A new implementation with O(logn) added

--- recursive-power.py
+++ recursive-power.py
@@ -3,7 +3,19 @@ Problem: Assuming that the exponent K is always a non-negative integer, write a 
 '''
 
 
+# O(n)
 def power(x, k):
     if k == 0:
         return 1
     return x * power(x, k-1)
+
+
+# O(log(n))
+def power2(x, k):
+    if k == 0:
+        return 1
+    partial = power(x, k//2)
+    result = partial * partial
+    if k % 2 == 1:
+        result *= x
+    return result
