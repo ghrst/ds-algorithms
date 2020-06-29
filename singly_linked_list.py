b604681748689ee3f114152fdc062b2d5d4ca18a
Gholamreza Sabery Tabrizy <reza_sabery_89@yahoo.com>
2020-06-29 11:04:36 +0430

    add_to_tail method added. Now we can use the list to implement a linked list based queue

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -28,19 +28,31 @@ class SLinkedList:
     
     def __init__(self):
         self._head = None
+        self._tail = None
         self._size = 0
     
     def __len__(self):
         return self._size
+        
+    def is_empty(self):
+        return self._size == 0
     
     def add_to_head(self, item):
         new = SLinkedList._Node(item)
         new.set_next(self._head)
         self._head = new
+        if self.is_empty():
+            self._tail = self._head
         self._size += 1
-        
-    def is_empty(self):
-        return self._size == 0
+    
+    def add_to_tail(self, item):
+        if self.is_empty():
+            self.add_to_head(item)
+            return
+        new = SLinkedList._Node(item)
+        self._tail.set_next(new)
+        self._tail = new
+        self._size += 1
     
     def remove_from_head(self):
         if self.is_empty():
@@ -48,16 +60,14 @@ class SLinkedList:
         node = self._head
         self._head = node.get_next()
         self._size -= 1
+        if node == self._tail:
+            self._tail = None
         return node.get_element()
     
     def peek(self):
         if self.is_empty():
             raise ListEmpty('The list is empty!')
         return self._head.get_element()
-    
-    
-    
-        
         
         
         
